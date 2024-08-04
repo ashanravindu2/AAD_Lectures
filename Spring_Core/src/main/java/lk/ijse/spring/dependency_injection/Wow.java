04298b749826c1a5f0cf6bda8a1fd64e79e669a2
Charaka V Mihiranga <143955766+CharakaMihiranga@users.noreply.github.com>
2024-08-04 15:05:26 +0530

    change the priority order of  spring beans

--- Spring_Core/src/main/java/lk/ijse/spring/dependency_injection/Wow.java
+++ Spring_Core/src/main/java/lk/ijse/spring/dependency_injection/Wow.java
@@ -7,5 +7,5 @@ import java.lang.annotation.RetentionPolicy;
 
 @Qualifier
 @Retention(RetentionPolicy.RUNTIME) // This annotation is used to indicate that this annotation should be available at runtime
-public @interface Wow {
+public @interface Wow { //Wow is a custom annotation that used to revamp the priority of the GoodGirl beans
 }
