311e25829781e3d79b0c08bf89e6b0fb936f9dc8
Charaka V Mihiranga <143955766+CharakaMihiranga@users.noreply.github.com>
2024-08-04 15:56:37 +0530

    Constructor injection and Setter injection implement

--- Spring_Core/src/main/java/lk/ijse/spring/dependency_injection/Wow.java
+++ Spring_Core/src/main/java/lk/ijse/spring/dependency_injection/Wow.java
@@ -6,6 +6,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Qualifier
-@Retention(RetentionPolicy.RUNTIME) // This annotation is used to indicate that this annotation should be available at runtime
-public @interface Wow { //Wow is a custom annotation that used to revamp the priority of the GoodGirl beans
+@Retention(RetentionPolicy.RUNTIME) // This annotation is used to indicate that this annotation should be available at runtime.
+public @interface Wow { //@interface Wow is a custom annotation that used to revamp the priority of the GoodGirl beans
 }
+
+// ** @Annotation ** //
+// Annotation is a special kind of interface that is used to provide metadata to the Java code.
+
+// ** RetentionPolicy ** //
+// https://www.geeksforgeeks.org/java-retention-annotations/
+// http s://www.baeldung.com/java-default-annoitations
--- Spring_Core/src/main/java/lk/ijse/spring/dependency_injection/user/Boy.java
+++ Spring_Core/src/main/java/lk/ijse/spring/dependency_injection/user/Boy.java
@@ -3,22 +3,58 @@ package lk.ijse.spring.dependency_injection.user;
 import jakarta.annotation.PostConstruct;
 import lk.ijse.spring.dependency_injection.Wow;
 import lk.ijse.spring.dependency_injection.dep.GoodGirl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-
 public class Boy {
 
-    @Autowired
+//    // ** Field Injection **=======================================================================================================
+
+    @Autowired //Autowired is used to inject the dependencies to the bean
     @Qualifier("properties")
     @Wow //Custom Annotation
     private GoodGirl goodGirl;
 
-    @PostConstruct // This annotation is used to indicate that this method should be executed after the instantiation of the bean
+//
+//
+//
+
+//    // ** Constructor Injection **=========================This is the Recommended Method=====================================
+//    @Qualifier("properties")
+//    private final GoodGirl goodGirl;
+//
+//    @Autowired
+//    @Wow
+//    public Boy(GoodGirl goodGirl) {
+//        this.goodGirl = goodGirl;
+//    }
+
+//    // ** Setter Injection **=======================================================================================================
+//    @Qualifier("properties")
+//    private GoodGirl goodGirl;
+//
+//    @Autowired
+//    @Wow
+//    public void setGoodGirl(GoodGirl goodGirl) {
+//        this.goodGirl = goodGirl;
+//    }
+
+    @PostConstruct // This annotation is used to indicate that this method should be executed after the instantiation of the bean,This is not a spring annotation it's a jakarta annotation
     public void init(){
 //        System.out.println(goodGirl);
         goodGirl.love();
     }
+
+
+
+
 }
+
+// ** Dependency Injection Types **
+// 1. Field Injection
+// 2. Constructor Injection
+// 3. Setter Injection
+// 4. Value Injection
