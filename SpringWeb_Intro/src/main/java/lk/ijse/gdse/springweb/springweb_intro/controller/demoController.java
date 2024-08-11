dbbabaa4768fcad09a849332f4acf981460d54bb
Charaka V Mihiranga <143955766+CharakaMihiranga@users.noreply.github.com>
2024-08-11 15:17:04 +0530

    patter regex with getMapping

--- SpringWeb_Intro/src/main/java/lk/ijse/gdse/springweb/springweb_intro/controller/demoController.java
+++ SpringWeb_Intro/src/main/java/lk/ijse/gdse/springweb/springweb_intro/controller/demoController.java
@@ -2,14 +2,30 @@ package lk.ijse.gdse.springweb.springweb_intro.controller;
 
 import jakarta.servlet.http.HttpServlet;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("api/v1/demo")  // using this annotation we can define the base URL for this controller
-@RestController  // using this annotation we can make this class as a controller
+@RequestMapping("/demo")
+//The @RequestMapping annotation in Spring Web is used to map web requests to specific handler classes and/or handler methods.
+// It can be applied at the class level and/or at the method level in a controller.
+@RestController  // The @RestController annotation in Spring Web is a specialized version of the @Controller annotation.
+// It is used to create RESTful web services. The @RestController annotation is a combination of @Controller and @ResponseBody,
+// which means that it not only marks the class as a controller but also ensures that the return values of the methods are automatically
+// serialized into JSON or XML and sent back to the client.
 public class DemoController {
-    @GetMapping(value = "/health")  // using this annotation we can define the URL for this method
-    public String health(){
-        return "I'm OK";
+//    @GetMapping(value = "/health")  // using this annotation we can define the URL for this method
+//    @GetMapping("/health/**") // using this mapping we can define the URL for this method
+    @GetMapping("/health/{name}")
+    public String healthCheck(@PathVariable("name") String name){  //http://localhost:8080/SpringWebIntro/demo/health/Demo
+        return "DemoController is running perfectly with: "+ name;
+    }
+    @GetMapping(value = "patternRegex/{id:C\\d{3}}") //http://localhost:8080/SpringWebIntro/demo/patternRegex/C001
+    public String testRegex(@PathVariable("id") String id){
+        return "Pattern Regex: "+id;
+    }
+    @GetMapping
+    public String otherTest(){
+        return "Other test";
     }
 }
