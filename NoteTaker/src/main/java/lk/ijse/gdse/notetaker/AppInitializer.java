7c809254ffed88d7be9e5ed09869611c6f9a20e7
Ashan Ravindu Lakshitha <139869812+ashanravindu2@users.noreply.github.com>
2024-08-18 12:50:14 +0530

    bug fix in JSONToDto and NoteTaker intialize

---
+++ NoteTaker/src/main/java/lk/ijse/gdse/notetaker/AppInitializer.java
@@ -0,0 +1,22 @@
+package lk.ijse.gdse.notetaker;
+
+import lk.ijse.gdse.notetaker.config.WebAppConfig;
+import lk.ijse.gdse.notetaker.config.WebAppRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebAppRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
