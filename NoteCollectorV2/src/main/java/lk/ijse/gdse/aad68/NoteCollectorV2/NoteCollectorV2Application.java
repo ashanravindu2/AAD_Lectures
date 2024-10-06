5ec3b8ca94f624ac9144668de2270d251f796da4
Ashan Ravindu Lakshitha <139869812+ashanravindu2@users.noreply.github.com>
2024-10-06 10:54:59 +0530

    Spring_gradle

---
+++ NoteCollectorV2/src/main/java/lk/ijse/gdse/aad68/NoteCollectorV2/NoteCollectorV2Application.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.aad68.NoteCollectorV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteCollectorV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteCollectorV2Application.class, args);
+	}
+
+}
---
+++ NoteCollectorV2/src/test/java/lk/ijse/gdse/aad68/NoteCollectorV2/NoteCollectorV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.aad68.NoteCollectorV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteCollectorV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
